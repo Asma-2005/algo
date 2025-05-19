8b6ce44b78576c1d50cffe00c1d72bcba2ccada7
YosefK7aled <148383263+YosefK7aled@users.noreply.github.com>
2025-05-20 02:41:22 +0300

    with Complexity & check it.

--- algo/Program.cs
+++ algo/Program.cs
@@ -12,7 +12,7 @@ public class BigInteger
     {
         digits = new List<byte> { 0 };
     }
-    public BigInteger(string number)
+    public BigInteger(string number)  //O(N) which N is the size of string.
     {
         digits = new List<byte>();
         for (int i = number.Length - 1; i >= 0; i--)
@@ -20,86 +20,84 @@ public class BigInteger
     }
 
     // Helper methods throughout the implementation
-    private void RemoveLeadingZeros()
+    private void RemoveLeadingZeros()          //overall complexty O(N)
     {
-        while (digits.Count > 1 && digits[^1] == 0)
-            digits.RemoveAt(digits.Count - 1);
+        while (digits.Count > 1 && digits[^1] == 0)            //O(N)
+            digits.RemoveAt(digits.Count - 1);                 // O(1)
     }
-    public bool LessThan(BigInteger other)
+    public bool LessThan(BigInteger other)     //overall complexty O(N)
     {
-        if (this.digits.Count != other.digits.Count)
-            return this.digits.Count < other.digits.Count;
-        for (int i = this.digits.Count - 1; i >= 0; i--)
+        if (this.digits.Count != other.digits.Count)      //O(1)
+            return this.digits.Count < other.digits.Count;//O(1)
+        for (int i = this.digits.Count - 1; i >= 0; i--)  //O(N)*1
         {
-            if (this.digits[i] != other.digits[i])
-                return this.digits[i] < other.digits[i];
+            if (this.digits[i] != other.digits[i])       //O(1)
+                return this.digits[i] < other.digits[i]; //O(1)
         }
         return false;
     }
-    public bool GreaterThan(BigInteger other)
+    public bool GreaterThan(BigInteger other)  //overall complexty O(N)
     {
-        if (this.digits.Count != other.digits.Count)
-            return this.digits.Count > other.digits.Count;
-        for (int i = this.digits.Count - 1; i >= 0; i--)
+        if (this.digits.Count != other.digits.Count)//O(1)
+            return this.digits.Count > other.digits.Count;//O(1)
+        for (int i = this.digits.Count - 1; i >= 0; i--)//O(N)*1
         {
-            if (this.digits[i] != other.digits[i])
-                return this.digits[i] > other.digits[i];
+            if (this.digits[i] != other.digits[i])//O(1)
+                return this.digits[i] > other.digits[i];//O(1)
         }
-        return false;
+        return false;//O(1)
     }
     public BigInteger powermod(BigInteger basee, BigInteger exp, BigInteger mod)
     {
-        BigInteger result = new BigInteger("1");
-        basee = BinaryDivMod(basee, mod).Remainder;
-
-        if (exp.Equals(new BigInteger("0")))
+        BigInteger result = new BigInteger("1");              
+        basee = BinaryDivMod(basee, mod).Remainder;      //O(N log N)    
+        if (exp.Equals(new BigInteger("0")))             //O(1)
         {
             return result;
         }
 
-        while (exp.GreaterThan(new BigInteger("0")))
+        while (exp.GreaterThan(new BigInteger("0")))   // O(logN⋅(NlogN+n ^1.584))-----> //O(logN)*(....+.....+....+....) overall O(n^1.584 log n) بفرض mul اكبر من  div
         {
-            BigInteger resultmod = BinaryDivMod(exp, new BigInteger("2")).Remainder;
-            string s = resultmod.ToString();
+            BigInteger resultmod = BinaryDivMod(exp, new BigInteger("2")).Remainder; // O( Nlog N)
+            string s = resultmod.ToString();                                     //O(1)
             string value1 = "1";               /// this string for get value from mod %2 and compare with 1 (odd);
             if (s.Equals(value1))
             {
                 //Console.WriteLine(exp.ToString());
-                result = result.Mul(basee);
-                result = BinaryDivMod(result, mod).Remainder;
+                result = result.Mul(basee);                                     //O(n^1.584)
+                result = BinaryDivMod(result, mod).Remainder;                   // O( Nlog N)
                 //Console.WriteLine(result.ToString());
             }
-            basee = basee.Mul(basee);
-            basee = BinaryDivMod(basee, mod).Remainder;
-            exp = BinaryDivMod(exp, new BigInteger("2")).Quotient;
+            basee = basee.Mul(basee);                                          //O(n^1.584)
+            basee = BinaryDivMod(basee, mod).Remainder;                        // O( Nlog N)
+            exp = BinaryDivMod(exp, new BigInteger("2")).Quotient;             // O( Nlog N)
             //Console.WriteLine(result.ToString());
         }
 
         return result;
     }
-    public BigInteger Encryption(BigInteger m, BigInteger e, BigInteger n)
+    public BigInteger Encryption(BigInteger m, BigInteger e, BigInteger n)  //Opowermod
     {
-        BigInteger encr = new BigInteger();
-        encr = powermod(m, e, n);
-        return encr;
+        BigInteger encr = powermod(m, e, n);                                //O() powermod
+        return encr;                                                        //θ(1)
     }
-    public BigInteger Decryption(BigInteger em, BigInteger d, BigInteger n)
+    public BigInteger Decryption(BigInteger em, BigInteger d, BigInteger n) //Opowermod
     {
-
-        BigInteger dencr = powermod(em, d, n);
-
-        return dencr;
+        BigInteger dencr = powermod(em, d, n);                              //O()  powermod
+        return dencr;                                                       //θ(1)
     }
-    public bool Equals(BigInteger other)
+    public bool Equals(BigInteger other)                                    // O(N)
     {
-        if (this.digits.Count != other.digits.Count)
-            return false;
-        for (int i = 0; i < this.digits.Count; i++)
-            if (this.digits[i] != other.digits[i])
-                return false;
-        return true;
+        //this.RemoveLeadingZeros();                                          //θ(N)
+        //other.RemoveLeadingZeros();                                         //θ(N)
+        if (this.digits.Count != other.digits.Count)                        //θ(1)
+            return false;                                                   //θ(1)
+        for (int i = 0; i < this.digits.Count; i++)                         //θ(N)
+            if (this.digits[i] != other.digits[i])                          //θ(1)
+                return false;                                               //θ(1)
+        return true;                                                        //θ(1)
     }
-    private BigInteger HalveValue()
+    private BigInteger HalveValue()    // O(N)
     {
         // Handle zero case
         if (this.Equals(new BigInteger("0")))
@@ -119,7 +117,7 @@ public class BigInteger
         result.RemoveLeadingZeros();
         return result;
     }
-    public override string ToString()
+    public override string ToString()//O(N)
     {
         StringBuilder sb = new StringBuilder();
         for (int i = digits.Count - 1; i >= 0; i--)
@@ -129,7 +127,7 @@ public class BigInteger
     public bool IsEven()
     {
         return digits[0] % 2 == 0;
-    }
+    } //O(1)
 
     public BigInteger Add(BigInteger other)
     {
@@ -146,7 +144,7 @@ public class BigInteger
             carry = sum / 10;
         }
         return result;
-    }
+    } //O(N)
     public BigInteger Sub(BigInteger other)
     {
         BigInteger result = new BigInteger();
@@ -169,7 +167,7 @@ public class BigInteger
         }
         result.RemoveLeadingZeros();
         return result;
-    }
+    } //O(N)
     // public BigInteger Mul(BigInteger other)
     // {
     //     BigInteger result = new BigInteger();
@@ -191,7 +189,7 @@ public class BigInteger
     // }
 
     // Binary long division and modulus
-    public static (BigInteger Quotient, BigInteger Remainder) BinaryDivMod(BigInteger dividend, BigInteger divisor)
+    public static (BigInteger Quotient, BigInteger Remainder) BinaryDivMod(BigInteger dividend, BigInteger divisor) //O(NlogN)u
     {
         if (divisor.Equals(new BigInteger("0")))
             throw new DivideByZeroException("Cannot divide by zero.");
@@ -232,14 +230,14 @@ public class BigInteger
 
         return (quotient, remainder);
     }
-    private static BigInteger ShiftLeft(BigInteger num, int n)
+    private static BigInteger ShiftLeft(BigInteger num, int n) //O(N+M)
     {
         BigInteger result = new BigInteger();
         result.digits = new List<byte>(new byte[n]);
         result.digits.AddRange(num.digits);
         return result;
     }
-    public BigInteger Mul(BigInteger other)
+    public BigInteger Mul(BigInteger other)//O(N^1.584)
     {
         BigInteger result = new BigInteger();
 
@@ -318,11 +316,143 @@ public class BigInteger
         return result;
     }
 
+
+    //bounse1 
+    public static StringBuilder turnToBigInt(string sentence)
+    {
+        sentence = sentence.ToLower();
+        StringBuilder numberString = new StringBuilder();
+        BigInteger bigInteger = new BigInteger();
+        foreach (char c in sentence)
+        {
+            if (char.IsLetter(c))
+            {
+                int num = c - 'a';
+                numberString.Append(num.ToString("D2")); //to be able to decode again(each char has a number from 0 to 25)
+            }
+        }
+
+        return numberString;
+    }
+
+    //bounse 2:
+
+    public static BigInteger GCD(BigInteger a, BigInteger b)
+    {
+        while (!b.Equals(new BigInteger("0")))
+        {
+            var temp = b;
+            b = BinaryDivMod(a, b).Remainder; //b is the reminder
+            a = temp;
+        }
+        return a;
+    }
+    public static BigInteger RandomPrime(int digits)
+    {
+        Random rand = new Random();
+        BigInteger numOne = new BigInteger("1");
+        BigInteger two = new BigInteger("2");
+
+        while (true)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(rand.Next(1, 10)); //from 1 to not start with zero, reham
+            for (int i = 1; i < digits; i++)
+                sb.Append(rand.Next(0, 10));
+
+            BigInteger n = new BigInteger(sb.ToString()); //turning n to number instead of string
+
+
+            if (n.IsEven())
+                n = n.Add(numOne);
+
+            // 3mo Fermat test to make sure the num is prime
+            BigInteger a = new BigInteger("2");
+            BigInteger n_1 = n.Sub(numOne);
+            BigInteger result = new BigInteger().powermod(a, n_1, n);
+
+
+            if (result.Equals(numOne))
+                return n; //it will break the loop and return teh n onlyy if n==prime number
+        }
+
+
+    }
+    public static (BigInteger e, BigInteger n) PublicKeyGenration()
+    {
+        BigInteger one = new BigInteger("1");
+
+        BigInteger p = RandomPrime(20);
+        BigInteger q = RandomPrime(20);
+
+        BigInteger p_1 = p.Sub(one);
+        BigInteger q_1 = q.Sub(one);
+
+        BigInteger n = p.Mul(q);
+        BigInteger phai = p_1.Mul(q_1);
+
+        BigInteger e = new BigInteger();
+
+        while (true)
+        {
+            e = BigInteger.RandomPrime(5);
+            if (GCD(e, phai).Equals(new BigInteger("1")))
+                break;
+        }
+
+        return (n, e);
+    }
 }
 class Program
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        int n = int.Parse(input);
+        BigInteger e_d = new BigInteger();
+        BigInteger m_Em = new BigInteger();
+        int type;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            //entering e or d
+            string s_e_d = Console.ReadLine();
+            e_d = new BigInteger(s_e_d);
+
+            //entering m or E(m) 
+            string s_m_Em = Console.ReadLine();
+            m_Em = new BigInteger(s_e_d);
+
+            string stype = Console.ReadLine();
+            type = int.Parse(stype);
+
+            if (type == 0)
+            {
+
+
+            }
+            else if (type == 1)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("invalid type entered");
+            }
+
+        }
+
+        /* testing for bounse2:
+
+         //generating 2 prime numbers done
+        BigInteger p = BigInteger.RandomPrime(20);
+        BigInteger q = BigInteger.RandomPrime(20);
+
+        Console.WriteLine("p = " + p.ToString());
+        Console.WriteLine("q = " + q.ToString());
+
+         */
         while (true)
         {
             Console.Clear();
